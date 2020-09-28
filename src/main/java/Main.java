dbf10b38ed7d69c54c1f9839cf92f18270f3b8cd
daehokimm <daehokimm@daehokimm-MacBookPro.local>
2020-09-28 14:12:50 +0900

    Processor 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,22 @@ import java.util.concurrent.Flow.*;
 
 public class Main {
     public static void main(String[] args) {
-        getTemperature("Seoul").subscribe(new TempSubscriber());
+
+//        getTemperature("Seoul").subscribe(new TempSubscriber());
+        getCelsiusTemperatures("Seoul").subscribe(new TempSubscriber());
     }
 
     private static Publisher<TempInfo> getTemperature(String town) {
         return subscriber -> subscriber.onSubscribe(
                 new TempSubscription(subscriber, town)
         );
+    }
+
+    private static Publisher<TempInfo> getCelsiusTemperatures(String town) {
+        return subscriber -> {
+            TempProcessor processor = new TempProcessor();
+            processor.subscribe(subscriber);
+            processor.onSubscribe(new TempSubscription(processor, town));
+        };
     }
 }
